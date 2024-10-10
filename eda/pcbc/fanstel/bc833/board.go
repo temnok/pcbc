51d13bf0cfa513e5a1d34f6542aa1e5b93bd9a03
Alex Temnok <temnok@gmail.com>
2024-10-09 17:15:18 -0700

    BC833: short board

--- eda/pcbc/fanstel/bc833/board.go
+++ eda/pcbc/fanstel/bc833/board.go
@@ -12,7 +12,12 @@ import (
 )
 
 var (
-	header = &lib.Component{
+	Board      = board(false)
+	ShortBoard = board(true)
+)
+
+func board(short bool) *lib.Component {
+	header := &lib.Component{
 		Components: lib.Components{
 			{
 				Transform: geom.MoveXY(-8.9, -2.9).RotateD(-90),
@@ -35,27 +40,99 @@ var (
 		},
 	}
 
-	pads = header.Squash().Pads.Centers()
+	pad := header.Squash().Pads.Centers()
 
-	pins = fanstel.BC833.Pads.Centers()
+	if short {
+		header = &lib.Component{
+			Components: lib.Components{
+				{
+					Transform: geom.MoveXY(-8.9, -2.9-2.54).RotateD(-90),
+					Components: lib.Components{
+						mph100imp40f.G_V_SP_x6,
+					},
+				},
+				{
+					Transform: geom.MoveXY(0, -15.5),
+					Components: lib.Components{
+						mph100imp40f.G_V_SP_x8,
+					},
+				},
+				{
+					Transform: geom.MoveXY(8.9, -2.9-2.54).RotateD(90),
+					Components: lib.Components{
+						mph100imp40f.G_V_SP_x6,
+					},
+				},
+			},
+		}
+	}
 
-	labelShift = geom.XY{2.54 / 0.8, 0}
-	labelScale = geom.XY{0.8, 1.3}
+	pin := fanstel.BC833.Pads.Centers()
 
-	shiftedBoard = &lib.Component{
+	labelShift := geom.XY{2.54 / 0.8, 0}
+	labelScale := geom.XY{0.8, 1.3}
+
+	tracks := eda.TrackPaths(
+		eda.Track{pin[8]}.DX(1.3).DY(3.5).DY(1.4).DX(-5.4).DY(1.5).YX(pad[0]),
+		eda.Track{pin[7]}.DX(0.7).DY(3.3).DX(-1).XY(pad[1]),
+		eda.Track{pin[6]}.DY(1.7).DX(-0.5).XY(pad[2]),
+		eda.Track{pin[1]}.XY(pad[3]),
+		eda.Track{pin[2]}.YX(pad[4]),
+		eda.Track{pin[3]}.YX(pad[5]),
+		eda.Track{pin[4]}.DX(-3.1).YX(pad[6]),
+
+		eda.Track{pin[21]}.DY(-2.7).DX(-4.6).DX(-1).DY(-7.6).DY(-1.9).XY(pad[8]),
+		eda.Track{pin[20]}.DY(-2.3).DX(-4.3).DX(-0.7).DY(-7.4).DY(-2.1).XY(pad[9]),
+		eda.Track{pin[9]}.DX(-0.4).YX(pad[10]),
+		eda.Track{pin[10]}.DX(-0.8).YX(pad[11]),
+		eda.Track{pin[11]}.DX(-1.2).YX(pad[12]),
+		eda.Track{pin[12]}.DX(-1.6).YX(pad[13]),
+		eda.Track{pin[13]}.DX(-2.0).DY(-8.8).DX(2.1).XY(pad[14]),
+		eda.Track{pin[14]}.DX(-2.4).DY(-8.2).DX(1.9).XY(pad[15]),
+
+		eda.Track{pin[19]}.DX(3.5).YX(pad[16]),
+		eda.Track{pin[15]}.DX(3.1).YX(pad[17]),
+		eda.Track{pin[16]}.YX(pad[18]),
+		eda.Track{pin[17]}.YX(pad[19]),
+		eda.Track{pin[18]}.XY(pad[20]),
+		eda.Track{pin[22]}.DY(0.7).XY(pad[21]),
+		eda.Track{pin[23]}.DY(1.3).XY(pad[22]),
+		eda.Track{pin[24]}.DY(1.9).DX(6.1).DY(1.5).YX(pad[23]),
+	)
+
+	leftLabels := []string{"P031", "P030", "P029", "VDD", "P003", "P002", "P028", "GND"}
+	centerLabels := []string{"P020", "P017", "P004", "P005", "P109", "P011", "VDDH", "VBUS"}
+	rightLabels := []string{"D+", "D-", "P015", "P018", "SWD", "SWC", "P009", "P010"}
+
+	if short {
+		tracks[0] = nil
+		tracks[1] = nil
+		tracks[21] = nil
+		tracks[22] = nil
+
+		leftLabels[0] = ""
+		leftLabels[1] = ""
+		rightLabels[6] = ""
+		rightLabels[7] = ""
+	}
+
+	leftMount := geom.XY{-5, -10.5}
+	rightMount := geom.XY{5, -10.5}
+
+	shiftedBoard := &lib.Component{
 		Components: lib.Components{
 			fanstel.BC833,
 
 			header,
 
 			{
-				Transform: geom.MoveXY(-5, -10.5),
+				Transform: geom.Move(leftMount),
 				Components: lib.Components{
 					pcbc.MountHole,
 				},
 			},
 			{
-				Transform: geom.MoveXY(5, -10.5),
+				Transform: geom.Move(rightMount),
 				Components: lib.Components{
 					pcbc.MountHole,
 				},
@@ -63,42 +140,18 @@ var (
 		},
 
 		Tracks: path.Strokes{
-			0: eda.TrackPaths(
-				eda.Track{pins[8]}.DX(1.3).DY(3.5).DY(1.4).DX(-5.4).DY(1.5).YX(pads[0]),
-				eda.Track{pins[7]}.DX(0.7).DY(3.3).DX(-1).XY(pads[1]),
-				eda.Track{pins[6]}.DY(1.7).DX(-0.5).XY(pads[2]),
-				eda.Track{pins[1]}.XY(pads[3]),
-				eda.Track{pins[2]}.YX(pads[4]),
-				eda.Track{pins[3]}.YX(pads[5]),
-				eda.Track{pins[4]}.DX(-3.1).YX(pads[6]),
-
-				eda.Track{pins[21]}.DY(-2.7).DX(-4.6).DX(-1).DY(-7.6).DY(-1.9).XY(pads[8]),
-				eda.Track{pins[20]}.DY(-2.3).DX(-4.3).DX(-0.7).DY(-7.4).DY(-2.1).XY(pads[9]),
-				eda.Track{pins[9]}.DX(-0.4).YX(pads[10]),
-				eda.Track{pins[10]}.DX(-0.8).YX(pads[11]),
-				eda.Track{pins[11]}.DX(-1.2).YX(pads[12]),
-				eda.Track{pins[12]}.DX(-1.6).YX(pads[13]),
-				eda.Track{pins[13]}.DX(-2.0).DY(-8.8).DX(2.1).XY(pads[14]),
-				eda.Track{pins[14]}.DX(-2.4).DY(-8.2).DX(1.9).XY(pads[15]),
-
-				eda.Track{pins[19]}.DX(3.5).YX(pads[16]),
-				eda.Track{pins[15]}.DX(3.1).YX(pads[17]),
-				eda.Track{pins[16]}.YX(pads[18]),
-				eda.Track{pins[17]}.YX(pads[19]),
-				eda.Track{pins[18]}.XY(pads[20]),
-				eda.Track{pins[22]}.DY(0.7).XY(pads[21]),
-				eda.Track{pins[23]}.DY(1.3).XY(pads[22]),
-				eda.Track{pins[24]}.DY(1.9).DX(6.1).DY(1.5).YX(pads[23]),
-			),
+			0: tracks,
 		},
 
 		GroundTracks: path.Strokes{
 			0: eda.TrackPaths(
-				eda.Track{pins[5]}.DX(-2.5).YX(pads[7]),
-				eda.Track{pins[5]}.DX(2).YX(pins[28]).XY(pins[26]).XY(pins[25]).YX(pins[27]).XY(pins[28]),
+				eda.Track{pin[5]}.DX(-2.5).YX(pad[7]),
+				eda.Track{pin[5]}.DX(2).YX(pin[28]).XY(pin[26]).XY(pin[25]).YX(pin[27]).XY(pin[28]),
 
-				eda.Track{{-5, -10.5}}.YX(pads[7]),
-				eda.Track{{5, -10.5}}.DX(1.3).DY(-1.3),
+				eda.Track{leftMount}.YX(pad[7]),
+				eda.Track{leftMount}.DX(1.5).DY(6.1).YX(pin[27]),
+				eda.Track{rightMount}.DX(-1.3).DY(1.3),
+				eda.Track{rightMount}.DX(1.3).DY(-1.3),
 			),
 		},
 
@@ -107,55 +160,41 @@ var (
 			font.CenterBold("BC833").Transform(geom.MoveXY(0, -10.4).ScaleK(2)),
 			font.CenterBold("nRF52833").Transform(geom.MoveXY(0, -12.2).ScaleK(1.5)),
 
-			font.CenterBolds([]string{
-				"P031",
-				"P030",
-				"P029",
-				"VDD",
-				"P003",
-				"P002",
-				"P028",
-				"GND",
-			}, labelShift).Transform(geom.MoveXY(-7.1, -2.9).RotateD(-90).Scale(labelScale)),
-			font.CenterBolds([]string{
-				"P020",
-				"P017",
-				"P004",
-				"P005",
-				"P109",
-				"P011",
-				"VDDH",
-				"VBUS",
-			}, labelShift).Transform(geom.MoveXY(0, -13.65).Scale(labelScale)),
-			font.CenterBolds([]string{
-				"D+",
-				"D-",
-				"P015",
-				"P018",
-				"SWD",
-				"SWC",
-				"P009",
-				"P010",
-			}, labelShift).Transform(geom.MoveXY(7.1, -2.9).RotateD(90).Scale(labelScale)),
+			font.CenterBolds(leftLabels, labelShift).
+				Transform(geom.MoveXY(-7.1, -2.9).RotateD(-90).Scale(labelScale)),
+			font.CenterBolds(centerLabels, labelShift).
+				Transform(geom.MoveXY(0, -13.65).Scale(labelScale)),
+			font.CenterBolds(rightLabels, labelShift).
+				Transform(geom.MoveXY(7.1, -2.9).RotateD(90).Scale(labelScale)),
 		),
 	}
 
-	Board = &lib.Component{
-		Clears: path.Paths{
-			path.Rect(21.5, 5.5).Transform(geom.MoveXY(0, 9.7)),
-		},
+	boardShift := 4.75
+	boardCut := path.RoundRect(21, 24.6, 1)
+	boardClears := path.Paths{
+		path.Rect(21.5, 5.5).Transform(geom.MoveXY(0, 9.7)),
+	}
+
+	if short {
+		boardShift += 2.54
+		boardCut = path.RoundRect(21, 19.5, 1)
+		boardClears = nil
+	}
+
+	return &lib.Component{
+		Clears: boardClears,
 
 		Cuts: path.Paths{
-			path.RoundRect(21, 24.6, 1),
+			boardCut,
 		},
 
 		Components: lib.Components{
 			{
-				Transform: geom.MoveXY(0, 4.75),
+				Transform: geom.MoveXY(0, boardShift),
 				Components: lib.Components{
 					shiftedBoard,
 				},
 			},
 		},
 	}
-)
+}
--- eda/pcbc/fanstel/bc833/board_test.go
+++ eda/pcbc/fanstel/bc833/board_test.go
@@ -3,13 +3,70 @@ package bc833
 import (
 	"github.com/stretchr/testify/assert"
 	"temnok/pcbc/eda"
+	"temnok/pcbc/eda/lib"
+	"temnok/pcbc/eda/pcbc"
+	"temnok/pcbc/geom"
+	"temnok/pcbc/path"
 	"testing"
 )
 
+func Test_BC833Normal(t *testing.T) {
+	pcb := eda.NewPCB(36, 46)
+	pcb.Component(&lib.Component{
+		Clears: path.Paths{path.Rect(36, 46)},
+		Components: lib.Components{
+			pcbc.Board35x45,
+			Board,
+		},
+	})
+	assert.NoError(t, pcb.SaveFiles("out/normal/"))
+}
+
+func Test_BC833NormalShort(t *testing.T) {
+	pcb := eda.NewPCB(36, 46)
+	pcb.Component(shortBoard(true))
+	assert.NoError(t, pcb.SaveFiles("out/normal-short/"))
+}
+
 func Test_BC833(t *testing.T) {
 	pcb := eda.NewPCB(36, 46)
+	pcb.Component(&lib.Component{
+		Components: lib.Components{
+			pcbc.Board35x45,
+			Board,
+		},
+	})
+	assert.NoError(t, pcb.SaveFiles("out/ground/"))
+}
 
-	pcb.Component(Board)
+func Test_BC833Short(t *testing.T) {
+	pcb := eda.NewPCB(36, 46)
+	pcb.Component(shortBoard(false))
+	assert.NoError(t, pcb.SaveFiles("out/ground-short/"))
+}
 
-	assert.NoError(t, pcb.SaveFiles("out/bc833-"))
+func shortBoard(clear bool) *lib.Component {
+	clears := path.Paths{path.Rect(36, 46)}
+	if !clear {
+		clears = nil
+	}
+
+	return &lib.Component{
+		Clears: clears,
+		Components: lib.Components{
+			pcbc.Board35x45,
+			&lib.Component{
+				Transform: geom.MoveXY(0, 10.5),
+				Components: lib.Components{
+					ShortBoard,
+				},
+			},
+			&lib.Component{
+				Transform: geom.MoveXY(0, -10.5).RotateD(180),
+				Components: lib.Components{
+					ShortBoard,
+				},
+			},
+		},
+	}
 }
