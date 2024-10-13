8ca830bda645770505ca1c777aeed56a22f0189e
Alex Temnok <temnok@gmail.com>
2024-10-13 15:59:50 -0700

    Minor fix

--- eda/lib/fanstel/bc833.go
+++ eda/lib/fanstel/bc833.go
@@ -40,10 +40,10 @@ var (
 			26: pad.Transform(geom.MoveXY(0.5, 1.8)),
 			27: pad.Transform(geom.MoveXY(-0.5, 2.8)),
 			28: pad.Transform(geom.MoveXY(-0.5, 1.8)),
-		}.Transform(geom.MoveXY(0, 2-14.3/2)),
+		}.Transform(geom.MoveXY(-0.15, 2.25-14.3/2)),
 
 		Marks: path.Strokes{
-			0.2: {
+			0.1: {
 				path.Rect(10, 14.3),
 			},
 		},
