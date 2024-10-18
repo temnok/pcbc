fc0faf4502170585ee178cac8945dd552c99db69
Alex Temnok <temnok@gmail.com>
2024-10-18 10:43:01 -0700

    Refactor

--- eda/pcbc/py32f002a/qfn16/board.go
+++ eda/pcbc/py32f002a/qfn16/board.go
@@ -12,74 +12,71 @@ import (
 )
 
 var (
-	chipTransform = geom.RotateD(45)
-	pins          = qfn.QFN16G.Pads.Transform(chipTransform).Centers()
-
-	headerTransform = geom.MoveXY(0, -4.25)
-	pads            = mph100imp40f.G_V_SP_x8.Pads.Transform(headerTransform).Centers()
-
-	headerWithTracks = &lib.Component{
+	chip = &lib.Component{
 		Components: lib.Components{
 			{
-				Transform: headerTransform,
+				Transform: geom.RotateD(45),
+				Components: lib.Components{
+					qfn.QFN16G,
+				},
+			},
+		},
+	}
+
+	pin = chip.Squash().Pads.Centers()
+
+	header = &lib.Component{
+		Components: lib.Components{
+			{
+				Transform: geom.MoveXY(0, -4.25),
 				Components: lib.Components{
 					mph100imp40f.G_V_SP_x8,
 				},
 			},
 		},
+	}
+
+	pad = header.Squash().Pads.Centers()
+
+	headerWithTracks = &lib.Component{
+		Components: lib.Components{header},
 		Tracks: path.Strokes{
 			0: eda.TrackPaths(
-				eda.Track{pads[0]}.Y(-2.5).X(-4.9).Y(-2).XY(pins[0]),
-				eda.Track{pads[1]}.Y(-2.5).XY(pins[1]),
-				eda.Track{pads[2]}.YX(pins[2]),
-				eda.Track{pads[3]}.X(-1.25).YX(pins[3]),
-				eda.Track{pads[4]}.X(1.25).YX(pins[4]),
-				eda.Track{pads[5]}.YX(pins[5]),
-				eda.Track{pads[6]}.Y(-2.5).XY(pins[6]),
-				eda.Track{pads[7]}.Y(-2.5).X(4.9).Y(-2).XY(pins[7]),
+				eda.Track{pad[0]}.Y(-2.5).X(-4.9).Y(-2).XY(pin[0]),
+				eda.Track{pad[1]}.Y(-2.5).XY(pin[1]),
+				eda.Track{pad[2]}.YX(pin[2]),
+				eda.Track{pad[3]}.X(-1.25).YX(pin[3]),
+				eda.Track{pad[4]}.X(1.25).YX(pin[4]),
+				eda.Track{pad[5]}.YX(pin[5]),
+				eda.Track{pad[6]}.Y(-2.5).XY(pin[6]),
+				eda.Track{pad[7]}.Y(-2.5).X(4.9).Y(-2).XY(pin[7]),
 			),
 		},
 		GroundTracks: path.Strokes{
 			0.16: eda.TrackPaths(
-				eda.Track{{9.5, 0}}.X(pins[16].X),
+				eda.Track{{9.5, 0}}.X(pin[16].X),
 			),
 		},
 	}
 
 	Board = &lib.Component{
-		Description: "PCBC PY32F002A QFN-16 board",
-
 		Components: lib.Components{
+			chip,
+			headerWithTracks,
 			{
-				Description: "Chip",
-				Transform:   chipTransform,
-				Components: lib.Components{
-					qfn.QFN16G,
-				},
-			},
-			{
-				Description: "Top Header",
-				Transform:   geom.RotateD(180),
+				Transform: geom.RotateD(180),
 				Components: lib.Components{
 					headerWithTracks,
 				},
 			},
 			{
-				Description: "Bottom Header",
-				Components: lib.Components{
-					headerWithTracks,
-				},
-			},
-			{
-				Description: "Left mount hole",
-				Transform:   geom.MoveXY(-7.5, 0),
+				Transform: geom.MoveXY(-7.5, 0),
 				Components: lib.Components{
 					pcbc.MountHole,
 				},
 			},
 			{
-				Description: "Right mount hole",
-				Transform:   geom.MoveXY(7.5, 0),
+				Transform: geom.MoveXY(7.5, 0),
 				Components: lib.Components{
 					pcbc.MountHole,
 				},
@@ -91,34 +88,16 @@ var (
 		},
 
 		Marks: path.Strokes{}.Append(
-			font.CenterBolds([]string{
-				"PA8",
-				"VCC",
-				"PB0",
-				"PA7",
-				"PA6",
-				"PA5",
-				"PA4",
-				"PA3",
-			}, geom.XY{2.54 / 0.9, 0}).Transform(geom.MoveXY(0, 2.4).ScaleXY(0.9, 1.2)),
-
-			font.CenterBolds([]string{
-				"PB1",
-				"PA12",
-				"SWD",
-				"SWC",
-				"PF2",
-				"PA0",
-				"PA1",
-				"PA2",
-			}, geom.XY{2.54 / 0.9, 0}).Transform(geom.MoveXY(0, -2.4).ScaleXY(0.9, 1.2)),
+			font.CenterBolds([]string{"PA8", "VCC", "PB0", "PA7", "PA6", "PA5", "PA4", "PA3"},
+				geom.XY{2.54 / 0.9, 0}).Transform(geom.MoveXY(0, 2.4).ScaleXY(0.9, 1.2)),
 
 			pcbc.Logo.Transform(geom.MoveXY(-9.7, 0).ScaleK(0.8)),
-			pcbc.TmnkTech.Transform(geom.MoveXY(9.7, 0).RotateD(90)),
 			font.CenterBold("PY32").Transform(geom.MoveXY(-4.2, 0).ScaleXY(1.3, 2.5)),
 			font.CenterBold("F002A").Transform(geom.MoveXY(4.2, 0).ScaleXY(1, 2.5)),
-		),
+			pcbc.TmnkTech.Transform(geom.MoveXY(9.7, 0).RotateD(90)),
 
-		Tracks: path.Strokes{},
+			font.CenterBolds([]string{"PB1", "PA12", "SWD", "SWC", "PF2", "PA0", "PA1", "PA2"},
+				geom.XY{2.54 / 0.9, 0}).Transform(geom.MoveXY(0, -2.4).ScaleXY(0.9, 1.2)),
+		),
 	}
 )
--- eda/pcbc/py32f002a/sop8/board.go
+++ eda/pcbc/py32f002a/sop8/board.go
@@ -11,109 +11,83 @@ import (
 	"temnok/pcbc/path"
 )
 
-var Board *lib.Component
+var (
+	chip = &lib.Component{
+		Transform:  geom.MoveXY(2.3, 0),
+		Components: lib.Components{sop.SOP8},
+	}
 
-func init() {
-	chipTransform := geom.MoveXY(2.3, 0)
-	pins := sop.SOP8.Pads.Transform(chipTransform).Centers()
+	pin = chip.Squash().Pads.Centers()
 
-	headerTransform := geom.MoveXY(0, -6)
-	pads := mph100imp40f.G_V_SP_x4.Pads.Transform(headerTransform).Centers()
-
-	headerWithTracks := &lib.Component{
+	header = &lib.Component{
 		Components: lib.Components{
 			{
-				Transform: headerTransform,
+				Transform: geom.MoveXY(0, -6),
 				Components: lib.Components{
 					mph100imp40f.G_V_SP_x4,
 				},
 			},
 		},
+	}
+
+	pad = header.Squash().Pads.Centers()
+
+	headerWithTracks = &lib.Component{
+		Components: lib.Components{header},
 		Tracks: path.Strokes{
 			0: eda.TrackPaths(
-				eda.Track{pads[1]}.Y(-4.5).X(pins[1].X).Y(-4).Y(pins[1].Y),
-				eda.Track{pads[2]}.X(pins[2].X).Y(pins[2].Y),
-				eda.Track{pads[3]}.Y(-4).X(pins[3].X).Y(pins[3].Y),
+				eda.Track{pad[1]}.Y(-4.5).X(pin[1].X).Y(-4).Y(pin[1].Y),
+				eda.Track{pad[2]}.X(pin[2].X).Y(pin[2].Y),
+				eda.Track{pad[3]}.Y(-4).X(pin[3].X).Y(pin[3].Y),
 			),
 		},
 	}
 
-	board := &lib.Component{
-		Description: "PCBC PY32F002A SOP-8 board",
+	Board = &lib.Component{
+		Cuts: path.Paths{
+			path.RoundRect(10.75, 15, 1),
+		},
 
 		Components: lib.Components{
+			chip,
+			headerWithTracks,
 			{
-				Description: "Chip",
-				Transform:   chipTransform,
-				Components: lib.Components{
-					sop.SOP8,
-				},
-			},
-			{
-				Description: "Bottom Header",
+				Transform: geom.ScaleXY(1, -1),
 				Components: lib.Components{
 					headerWithTracks,
 				},
 			},
 			{
-				Description: "Top Header",
-				Transform:   geom.ScaleXY(1, -1),
-				Components: lib.Components{
-					headerWithTracks,
-				},
-			},
-			{
-				Description: "Mount hole",
-				Transform:   geom.MoveXY(-2.2, 0),
+				Transform: geom.MoveXY(-2.2, 0),
 				Components: lib.Components{
 					pcbc.MountHole,
 				},
 			},
 		},
 
-		Cuts: path.Paths{
-			path.RoundRect(10.75, 15, 1),
-		},
-
 		Marks: path.Strokes{}.Append(
+			font.CenterBolds([]string{"GND", "PA1", "PA2", "SWD"}, geom.XY{2.54 / 0.9, 0}).
+				Transform(geom.MoveXY(0, 4).ScaleXY(0.9, 1.2)),
+
 			font.CenterBold("PY32").Transform(geom.MoveXY(-2.8, 2.4).ScaleXY(1.4, 2)),
-			font.CenterBold("F002A").Transform(geom.MoveXY(-2.8, -2.4).ScaleXY(1.2, 2)),
 			pcbc.Logo.Transform(geom.MoveXY(-4.5, 0).ScaleK(0.8)),
+			font.CenterBold("F002A").Transform(geom.MoveXY(-2.8, -2.4).ScaleXY(1.2, 2)),
+
+			font.CenterBolds([]string{"VCC", "PA4", "PA3", "SWC"}, geom.XY{2.54 / 0.9, 0}).
+				Transform(geom.MoveXY(0, -4).ScaleXY(0.9, 1.2)),
 		),
 
 		Tracks: path.Strokes{
 			0: eda.TrackPaths(
-				eda.Track{pads[0]}.Y(-4).X(pins[0].X).Y(-3.5).Y(pins[0].Y),
+				eda.Track{pad[0]}.Y(-4).X(pin[0].X).Y(-3.5).Y(pin[0].Y),
 			),
 		},
 
 		GroundTracks: path.Strokes{
 			0: eda.TrackPaths(
-				eda.Track{{pads[0].X, -pads[0].Y}}.Y(4).X(pins[7].X).Y(3.5).Y(pins[7].Y),
-				eda.Track{pins[7]}.Y(0).X(-0.4).X(-2.2),
+				eda.Track{{pad[0].X, -pad[0].Y}}.Y(4).X(pin[7].X).Y(3.5).Y(pin[7].Y),
+				eda.Track{pin[7]}.Y(0).X(-0.4).X(-2.2),
 			),
 		},
 	}
-
-	pinNames := []string{
-		"VCC",
-		"PA4",
-		"PA3",
-		"SWC",
-		"SWD",
-		"PA2",
-		"PA1",
-		"GND",
-	}
-
-	const tenth = 2.54
-
-	for i := 0; i < 4; i++ {
-		board.Marks = board.Marks.Append(
-			font.CenterBold(pinNames[i]).Transform(geom.MoveXY(tenth*(float64(i)-1.5), -4).ScaleXY(0.9, 1.2)),
-			font.CenterBold(pinNames[7-i]).Transform(geom.MoveXY(tenth*(float64(i)-1.5), 4).ScaleXY(0.9, 1.2)),
-		)
-	}
-
-	Board = board
-}
+)
