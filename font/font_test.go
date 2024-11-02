e77756e875765c79f61d02590ed784eb946116d0
Alex Temnok <temnok@gmail.com>
2024-11-01 17:05:45 -0700

    Minor font refactoring

--- font/font_test.go
+++ font/font_test.go
@@ -13,17 +13,25 @@ import (
 func TestFont_SavePng(t *testing.T) {
 	const scale = 200.0
 
-	bm := bitmap.NewBitmap(16*scale*Width, 6*scale)
-	brush := shape.Circle(Normal * scale)
+	bm := bitmap.NewBitmap(16*scale*Width, 20*scale)
+	thinBrush := shape.Circle(Thin * scale)
+	normalBrush := shape.Circle(Normal * scale)
+	boldBrush := shape.Circle(Bold * scale)
 
 	for i := 0; i < 14; i++ {
 		for j := 0; j < 16; j++ {
 			c := (i+2)*16 + j
 
-			t := transform.Move(float64(j)*Width, float64(-i)).Scale(scale, -scale)
-			brush.IterateContours(Paths[c].Apply(t), bm.Set1)
+			tx := transform.Move(float64(j)*Width, float64(-i)).Scale(scale, -scale)
+			thinBrush.IterateContours(Paths[c].Apply(tx), bm.Set1)
+
+			tx = transform.Move(float64(j)*Width, float64(-i-7)).Scale(scale, -scale)
+			normalBrush.IterateContours(Paths[c].Apply(tx), bm.Set1)
+
+			tx = transform.Move(float64(j)*Width, float64(-i-14)).Scale(scale, -scale)
+			boldBrush.IterateContours(Paths[c].Apply(tx), bm.Set1)
 		}
 	}
 
-	assert.NoError(t, util.SavePng("tmp/font.png", bm.ToImage(color.Black, color.White)))
+	assert.NoError(t, util.SavePng("ascii/font.png", bm.ToImage(color.Black, color.White)))
 }
