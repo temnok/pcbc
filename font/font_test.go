b1f80098cd89829fea4483dc3856c20b455f81fc
Alex Temnok <temnok@gmail.com>
2025-04-29 14:16:25 -0700

    Refactor

--- font/font_test.go
+++ font/font_test.go
@@ -47,16 +47,16 @@ func TestFont_SavePng(t *testing.T) {
 
 func _TestConvertX(t *testing.T) {
 	for i := 0x20; i < 0x7f; i++ {
-		strokes := [][]int8{{}}
-		for _, j := range data[i] {
-			if j < 0 {
-				j = -j
-				strokes = append(strokes, nil)
-			}
-
-			n := len(strokes) - 1
-			strokes[n] = append(strokes[n], j-1)
-		}
+		strokes := [][]byte{{}}
+		//for _, j := range data[i] {
+		//	if j < 0 {
+		//		j = -j
+		//		strokes = append(strokes, nil)
+		//	}
+		//
+		//	n := len(strokes) - 1
+		//	strokes[n] = append(strokes[n], j+11)
+		//}
 		sort.Slice(strokes, func(i, j int) bool {
 			return strokes[i][0] < strokes[j][0]
 		})
@@ -64,20 +64,28 @@ func _TestConvertX(t *testing.T) {
 		fmt.Printf("'%c': {", i)
 		for i, s := range strokes {
 			n := len(s)
-			if n > 0 && s[0] > s[n-1] {
+			if n == 0 {
+				continue
+			}
+			if s[0] > s[n-1] {
 				for l, r := 0, n-1; l < r; l, r = l+1, r-1 {
 					s[l], s[r] = s[r], s[l]
 				}
 			}
+			if i > 0 {
+				fmt.Print(", ")
+			}
+			fmt.Print("{")
 			for j, b := range s {
-				if i > 0 || j > 0 {
+				if j > 0 {
 					fmt.Print(", ")
 				}
-				if i > 0 && j == 0 {
+				if b < 0 {
 					b = -b
 				}
 				fmt.Print(b)
 			}
+			fmt.Print("}")
 		}
 		fmt.Println("},")
 	}
