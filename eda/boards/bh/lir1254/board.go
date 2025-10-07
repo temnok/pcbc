2c6b46fa977fd7094566be502f20da7d013c14bb
Alex Temnok <temnok@gmail.com>
2025-10-07 15:05:26 -0700

    LIR1254: add distance between battery and contacts

--- eda/boards/bh/lir1254/board.go
+++ eda/boards/bh/lir1254/board.go
@@ -13,12 +13,12 @@ import (
 )
 
 var (
-	conn = greenconn.CSCC118(11, true, append([]string{""}, util.Repeat("3V7", 9)...)).
-		Arrange(transform.RotateDegrees(90).Move(0, -4.4))
+	conn = greenconn.CSCC118(13, true, util.Repeat("3V7", 13)).
+		Arrange(transform.RotateDegrees(90).Move(0, -5.2))
 
 	connPads = conn.PadCenters()
 
-	hold     = holder.LIR1254.Arrange(transform.Move(0, 4))
+	hold     = holder.LIR1254.Arrange(transform.Move(0, 4.2))
 	holdPads = hold.PadCenters()
 
 	Board = &eda.Component{
@@ -28,17 +28,17 @@ var (
 		Nested: eda.Components{
 			(&eda.Component{
 				Tracks: path.Paths{
-					eda.LinearTrack(holdPads[0], connPads[5].Move(0, -1), 0, 0, 0.9, -6, -1e-9),
+					eda.LinearTrack(holdPads[0], connPads[7].Move(0, -1), 0, 0, 0.9, -7, -1e-9),
 				},
 			}).Clone(2, transform.MirrorX),
 
 			{
 				Cuts: path.Paths{
-					path.RoundRect(18, 14.5, 2),
+					path.RoundRect(18, 16, 3),
 				},
 			},
 
-			boards.MountHoleV2.Arrange(transform.Move(0, -1.5)).CloneX(2, 12),
+			boards.MountHoleV2.Arrange(transform.Move(0, -1.3)).CloneX(2, 12),
 
 			conn,
 
@@ -50,11 +50,7 @@ var (
 
 			boards.Rev(2025, 10, 1).Arrange(transform.RotateDegrees(90).Scale(0.8, 0.8).Move(8.35, -1.5)),
 
-			eda.CenteredText("LIR").Arrange(transform.Scale(1.5, 2).Move(-7.3, -4)),
-			eda.CenteredText("1254").Arrange(transform.Scale(1.1, 2).Move(-7.3, -5.5)),
-
-			eda.CenteredText("3.7").Arrange(transform.Scale(1.5, 2).Move(7.3, -4)),
-			eda.CenteredText("V").Arrange(transform.Scale(2, 2).Move(7.3, -5.5)),
+			eda.CenteredText("LIR1254").Arrange(transform.Scale(2, 1.5).Move(0, -2.2)),
 		},
 	}
 )
