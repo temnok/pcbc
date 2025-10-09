dc76428332bbfcc8ea6469da3926e2e6358c1a70
Alex Temnok <temnok@gmail.com>
2025-10-09 03:30:15 -0700

    Minor change

---
+++ eda/boards/p2/resistors_leds/board.go
@@ -0,0 +1,49 @@
+// Copyright © 2025 Alex Temnok. All rights reserved.
+
+package resistors
+
+import (
+	"temnok/pcbc/eda"
+	"temnok/pcbc/eda/boards"
+	"temnok/pcbc/eda/boards/p2"
+	"temnok/pcbc/eda/boards/ts/ts026a"
+	"temnok/pcbc/path"
+)
+
+var Board = &eda.Component{
+	Nested: eda.Components{
+		{
+			Cuts: path.Paths{
+				path.RoundRect(34, 40, 1.4),
+			},
+		},
+
+		boards.AlignHole.CloneX(2, 30.5).CloneY(2, 36.5),
+
+		eda.ComponentGrid(3, 10.9, 4.9,
+			p2.P2_I0402("+LR", "-2V"),
+			p2.P2_I0402("+LG", "-3V"),
+			p2.P2_I0402("+LB", "-3V"),
+			p2.P2_I0402("+LY", "-2V"),
+			p2.P2_I0402("+LW", "-3V"),
+
+			p2.P2_I0402(" R ", "51R"),
+			p2.P2_I0402(" R ", "51R"),
+			p2.P2_I0402(" R ", "75R"),
+			p2.P2_I0402(" R ", "75R"),
+			p2.P2_I0402(" R ", "K10"),
+			p2.P2_I0402(" R ", "K10"),
+			p2.P2_I0402(" R ", "K10"),
+			p2.P2_I0402(" R ", "K15"),
+			p2.P2_I0402(" R ", "K15"),
+			p2.P2_I0402(" R ", "K20"),
+			p2.P2_I0402(" R ", "K20"),
+
+			ts026a.Board,
+			ts026a.Board,
+			ts026a.Board,
+			ts026a.Board,
+			ts026a.Board,
+		),
+	},
+}
---
+++ eda/boards/p2/resistors_leds/board_test.go
@@ -0,0 +1,17 @@
+// Copyright © 2025 Alex Temnok. All rights reserved.
+
+package resistors
+
+import (
+	"github.com/stretchr/testify/assert"
+	"temnok/pcbc/eda/pcb"
+	"temnok/pcbc/eda/pcb/config"
+	"testing"
+)
+
+func TestBoard(t *testing.T) {
+	conf := config.Default()
+	conf.Width, conf.Height = 35, 41
+
+	assert.NoError(t, pcb.Process(conf, Board))
+}
