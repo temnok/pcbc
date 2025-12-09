a8281c843d1d981da0b2b62349fe13c500418eb5
Alex Temnok <temnok@gmail.com>
2025-12-08 22:26:46 -0800

    Refactor

--- bezier/rasterize.go
+++ bezier/rasterize.go
@@ -10,8 +10,8 @@ import "math"
 // Cubic Bézier curve is represented by four points in xy array in form [Ax, Ay, Bx, By, Cx, Cy, Dx, Dy]
 // where A and D are start and end points of the curve and B and C are control points.
 func Rasterize(xy []float64, callback func(x, y int)) {
-	ax, ay := round(xy[0]), round(xy[1])
-	dx, dy := round(xy[6]), round(xy[7])
+	ax, ay := round(xy[0], xy[1])
+	dx, dy := round(xy[6], xy[7])
 
 	recurse(xy, 0, 1, ax, ay, dx, dy, callback)
 }
@@ -27,11 +27,26 @@ func recurse(xy []float64, t0, t1 float64, x0, y0, x1, y1 int, callback func(x, 
 	}
 
 	t := (t0 + t1) / 2
-	tX, tY := cubicBezier(xy, t)
-	x, y := round(tX), round(tY)
+	x, y := round(cubicApprox(xy, t))
 
 	recurse(xy, t0, t, x0, y0, x, y, callback)
 	recurse(xy, t, t1, x, y, x1, y1, callback)
+}
+
+func cubicApprox(xy []float64, t float64) (x, y float64) {
+	aX, aY, bX, bY, cX, cY, dX, dY := xy[0], xy[1], xy[2], xy[3], xy[4], xy[5], xy[6], xy[7]
+	abX, abY := mix(aX, aY, bX, bY, t)
+	bcX, bcY := mix(bX, bY, cX, cY, t)
+	cdX, cdY := mix(cX, cY, dX, dY, t)
+
+	abcX, abcY := mix(abX, abY, bcX, bcY, t)
+	bcdX, bcdY := mix(bcX, bcY, cdX, cdY, t)
+
+	return mix(abcX, abcY, bcdX, bcdY, t)
+}
+
+func mix(x0, y0, x1, y1, i float64) (float64, float64) {
+	return x0*(1-i) + x1*i, y0*(1-i) + y1*i
 }
 
 func abs(a int) int {
@@ -41,6 +56,6 @@ func abs(a int) int {
 	return a
 }
 
-func round(a float64) int {
-	return int(math.Round(a))
+func round(x, y float64) (int, int) {
+	return int(math.Round(x)), int(math.Round(y))
 }
